3e1d877dd244ae524b7e47da61954a6884ea2f43
Daniele Scasciafratte <mte90net@gmail.com>
2020-03-19 15:12:48 +0100

    fix path enqueue

--- .bash/export.sh
+++ .bash/export.sh
@@ -7,16 +7,16 @@ export WP_TESTS_DB_PASSWORD='test'
 export GTK_USE_PORTAL=1
 
 # Fix issues with pulse on my system
-PULSE_DIR="/tmp/$( whoami )-pulse"
-mkdir -p $PULSE_DIR && chmod 777 $PULSE_DIR && chown mte90:mte90 $PULSE_DIR
-export PULSE_CONFIG_PATH=$PULSE_DIR
-export PULSE_STATE_PATH=$PULSE_DIR
-export PULSE_RUNTIME_PATH=$PULSE_DIR
+#PULSE_DIR="/home/mte90/.config/pulse" #"/tmp/$( whoami )-pulse"
+#mkdir -p $PULSE_DIR && chmod 777 $PULSE_DIR && chown mte90:mte90 $PULSE_DIR
+#export PULSE_CONFIG_PATH=$PULSE_DIR
+#export PULSE_STATE_PATH=$PULSE_DIR
+#export PULSE_RUNTIME_PATH=$PULSE_DIR
 
 # Fix issues with permissions on my system
 export XDG_RUNTIME_DIR="/run/user/1000"
 
-export PATH=/usr/local/sbin$PATH
+export PATH=/usr/local/sbin:$PATH
 export PATH=./vendor/bin:$PATH
 export PATH=./composer/bin:$PATH
 export PATH=~/.composer/vendor/bin:$PATH
