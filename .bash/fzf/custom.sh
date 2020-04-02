7a9ea0c60d99b3568b92042c06af98b107ba962d
Daniele Scasciafratte <mte90net@gmail.com>
2020-04-02 16:08:44 +0200

    fzf updates

--- .bash/fzf/custom.sh
+++ .bash/fzf/custom.sh
@@ -1,6 +1,7 @@
 # https://github.com/junegunn/fzf
+# https://medium.com/@vdeantoni/boost-your-command-line-productivity-with-fuzzy-finder-985aa162ba5d
 export FZF_DEFAULT_COMMAND='ag --ignore-dir .sass-cache --ignore-dir _output --ignore-dir node_modules --ignore-dir _generated --ignore _bootstrap.php --ignore-dir vendor -g "" -U --nogroup --column --nocolor --php --html --css --js'
-export FZF_DEFAULT_OPTS='--exact --preview "bat --style=numbers --color=always {}"'
+export FZF_DEFAULT_OPTS='--exact --preview "([[ -f {} ]] && (batcat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200" --height=90%'
 # Bind F1 to open file to Kate, Ctrl-Y to copy to the clipboard the path, Ctrl-N to enter the folder
 alias fzf='fzf --bind "f1:execute(kate {}),ctrl-y:execute-silent(echo {} | pbcopy)+abort,ctrl-n:execute(cd {})"' 
 
@@ -18,4 +19,13 @@ function git-checkout() {
       git checkout "$result"
     fi
   fi
+}
+
+# find-in-file - usage: fif <SEARCH_TERM>
+fif() {
+  if [ ! "$#" -gt 0 ]; then
+    echo "Need a string to search for!";
+    return 1;
+  fi  
+  rg --files-with-matches --no-messages "$1" | fzf $FZF_PREVIEW_WINDOW --preview "rg --ignore-case --pretty --context 10 '$1' {}"
 }
