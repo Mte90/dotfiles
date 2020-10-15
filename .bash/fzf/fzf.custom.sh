7ff3c9ef3302764a8f2bfde54b8c680264f2c562
Daniele Scasciafratte <mte90net@gmail.com>
2020-10-15 16:21:10 +0200

    cleaning

--- .bash/fzf/fzf.custom.sh
+++ .bash/fzf/fzf.custom.sh
@@ -1,23 +1,18 @@
 # https://github.com/junegunn/fzf
 export FZF_DEFAULT_COMMAND="rg --files --no-ignore --follow -g '!{.sass\-cache,node_modules,_generated,_bootstrap.php,vendor}/*' 2> /dev/null"
+# https://medium.com/@vdeantoni/boost-your-command-line-productivity-with-fuzzy-finder-985aa162ba5d
+export FZF_DEFAULT_OPTS='--exact --preview "([[ -f {} ]] && (batcat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200" --height=90%'
 export FZF_DEFAULT_OPTS='--exact --preview "batcat --style=numbers --color=always {}"'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 bind -x '"\C-p": nvim-qt $(fzf);'
 # Bind F1 to open file to Kate, Ctrl-Y to copy to the clipboard the path, Ctrl-N to enter the folder
 alias fzf='fzf --bind "f1:execute(kate {}),ctrl-y:execute-silent(echo {} | pbcopy)+abort,ctrl-n:execute(cd {})"' 
 
-# Based on https://revelry.co/terminal-workflow-fzf/
-function git-checkout() {
-    result=$(git branch --color=always | grep -v '/HEAD\s' | sort |
-    fzf --height 50% --border --ansi --tac --preview-window right:70% \
-      --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
-    sed 's/^..//' | cut -d' ' -f1)
-
-  if [[ $result != "" ]]; then
-    if [[ $result == remotes/* ]]; then
-      git checkout --track $(echo $result | sed 's#remotes/##')
-    else
-      git checkout "$result"
-    fi
-  fi
+# find-in-file - usage: fif <SEARCH_TERM>
+find-in-file() {
+  if [ ! "$#" -gt 0 ]; then
+    echo "Need a string to search for!";
+    return 1;
+  fi  
+  rg --files-with-matches --no-messages "$1" | fzf $FZF_PREVIEW_WINDOW --preview "rg --ignore-case --pretty --context 10 '$1' {}"
 }
