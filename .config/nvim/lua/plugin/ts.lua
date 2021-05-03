70eb143f91b5b6cff63e9895d76799075afeb2e1
Daniele Scasciafratte <mte90net@gmail.com>
2021-05-03 15:21:39 +0200

    feat(nvim): other nvim migration

--- .config/nvim/lua/plugin/ts.lua
+++ .config/nvim/lua/plugin/ts.lua
@@ -13,6 +13,7 @@ require'nvim-treesitter.configs'.setup {
 local opts = {
     highlight_hovered_item = true,
     show_guides = true,
+    auto_preview = true
 }
 
 require('symbols-outline').setup(opts)
